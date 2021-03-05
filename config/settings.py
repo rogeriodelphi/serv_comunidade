7d0c1b415672be1631c9dd464d0fabb440834690
rogeriodelphi <rogeriodelphi@gmail.com>
2021-03-05 18:15:28 -0300

    Configurando os arquivos para o deploy no Heroku

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+
+from dj_database_url import parse as dburl
+from decouple import config
+
+from dj_static import Cling
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ex58vl(9-+$8qs*9pz2iv3qf^)5akk5)nvou*lmakrk1!2#t_3'
+SECRET_KEY = config( 'SECRET_KEY' )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config( 'DEBUG' , default = False , cast = bool )
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['serv-comunidade-rbm.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -76,11 +83,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+"""
+
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3' )
+
+DATABASES = {
+    'default' : config( 'DATABASE_URL' , default =default_dburl, cast =dburl),
 }
 
 # Password validation
